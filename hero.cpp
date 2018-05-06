2bc2b5b09b6fa724f985a0e036370b0ac7218d89
zliang <31651745+zliang217@users.noreply.github.com>
2018-05-06 10:11:39 -0700

    Add files via upload

--- hero.cpp
+++ hero.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 bool hasCorrectSyntax(string song);
-bool isTranslatable(string song);
-bool isColor(char c);
-string translation(string song);
-int numBeat(string song);
+bool isTranslatable(string song); //verify whether the song is translatable
+bool isColor(char c); //verify whether a certain character is a color
+string translation(string song); //input song, return the translation of the song
+int numBeat(string song); //input a string, count how many beats are in that string
 int translateSong(string song, string& instructions, int& badBeat);
 
 int main() {
@@ -31,7 +31,10 @@ int main() {
 
 bool hasCorrectSyntax(string song){
     bool correctness=false;
-    if (!isColor(song[0]) && song[0]!='/'){
+    if (song==""){ //empty string true by default
+        return true;
+    }
+    else if (!isColor(song[0]) && song[0]!='/'){ //Given an unempty string, the first char can only be color or '/'
         return false;
     }
     for (int i=0; i<song.size()-1; i++){
@@ -46,11 +49,11 @@ bool hasCorrectSyntax(string song){
         }
         else if (isdigit(song[i])){
             //the only allowed characters after it are a single digit and '/'
-            if (song[i+1]=='/'){
+            if (song[i+1]=='/'){ //if it is '/', true by default
                 correctness=true;
             }
-            else if (isdigit(song[i+1]) && !isdigit(song[i+2]) && i!=song.size()-2){
-                correctness=true;
+            else if (isdigit(song[i+1]) && !isdigit(song[i+2]) && i!=song.size()-2){ //there can only be consecutive two digits, and make sure that the digits are not out of bound
+                correctness=true; //which might be redundant, but doesn't hurt anything
             }
             else{
                 return false;
@@ -60,7 +63,7 @@ bool hasCorrectSyntax(string song){
             return false;
         }
     }
-    correctness=(song[song.size()-1]=='/');
+    correctness=(song[song.size()-1]=='/'); //the last char must be a '/'
     return correctness;
 }
 
@@ -73,13 +76,13 @@ bool isTranslatable(string song){
             if(isdigit(song[i])){
                 int digit;
                 if(isdigit(song[i+1])) { //no need to worry about boundary problems since it has correct syntax already
-                    digit=10*(int(song[i])-48)+(int(song[i+1])-48);
+                    digit=10*(int(song[i])-48)+(int(song[i+1])-48); //use ASCII code conversion to translate the chars to their corresponding integer values
                     if(digit<2){
                         return false;
                     }
                     
                     for(int j=2;j<=digit;j++){
-                        if(i+1+digit>song.size()-1 || song[j+i]!='/'){
+                        if(i+1+digit>song.size()-1 || song[j+i]!='/'){ //while sustained note is in effect, some non-slash beat sneaks in
                             return false;
                         }
                     }
@@ -87,7 +90,7 @@ bool isTranslatable(string song){
                     
                 }
                 else{
-                    digit=int(song[i])-48;
+                    digit=int(song[i])-48; //similar to above, in the case of one digit
                     if(digit<2){
                         return false;
                     }
@@ -104,13 +107,13 @@ bool isTranslatable(string song){
 }
 
 bool isColor(char c){
-    if (c=='G' || c=='g' || c=='R' || c=='r' || c=='Y'|| c=='y' || c=='B' || c=='b' || c=='O' || c=='o'){
+    if (c=='G' || c=='g' || c=='R' || c=='r' || c=='Y'|| c=='y' || c=='B' || c=='b' || c=='O' || c=='o'){// only allowed letters for a color
         return true;
     }
     return false;
 }
 
-int numBeat(string song){
+int numBeat(string song){ //count '/' for counting beat numbers, and this is only used when the song has correct syntax, so no need to worry about weird characters
     int count = 0;
     for (int i=0;i<song.size();i++){
         if(song[i]=='/'){
@@ -121,12 +124,12 @@ int numBeat(string song){
 }
 
 int translateSong(string song, string& instructions, int& badBeat){
-    if(isTranslatable(song)){
+    if(isTranslatable(song)){ //if the song is translatable, give the translation
         instructions=translation(song);
         return 0;
     }
     
-    if(! hasCorrectSyntax(song)){
+    if(! hasCorrectSyntax(song)){ //if the song has incorrect syntax, give up and return 1
         return 1;
     }
     
@@ -139,8 +142,8 @@ int translateSong(string song, string& instructions, int& badBeat){
                     badBeat=numBeat(song.substr(0,i+2))+1; //song[i+2] will definitely be / since song doesn't end prematurely
                     return 2;
                 }
-                    
-                else if(i+1+digit<=song.size()-1) {
+                
+                else if(i+1+digit<=song.size()-1) { //get the number of the abrupt beat when sustained note is in effect
                     for(int j=2;j<digit;j++){
                         if(song[j+i]!='/'){
                             badBeat=numBeat(song.substr(0,i+j+1))+1;
@@ -148,14 +151,14 @@ int translateSong(string song, string& instructions, int& badBeat){
                         }
                     }
                 }
-                    
-                else if(i+1+digit>song.size()-1){
+                
+                else if(i+1+digit>song.size()-1){ //end prematurely
                     badBeat=int(song.size())+1;
                     return 4;
                 }
                 i++;
             }
-            else if (!isdigit(song[i+1])){
+            else if (!isdigit(song[i+1])){ //similar to above, just this time the case of a single digit
                 digit=int(song[i])-48;
                 if(digit<2){
                     badBeat=numBeat(song.substr(0,i+1))+1;
@@ -181,7 +184,7 @@ int translateSong(string song, string& instructions, int& badBeat){
 string translation(string song){ //since we use this function only when song is translatable, we don't need to worry about the erraneous situations
     int digit;
     string newSong = "";
-    if (song==""){
+    if (song==""){// if the song is empty, just return the empty string
         return song;
     }
     else{
@@ -199,13 +202,13 @@ string translation(string song){ //since we use this function only when song is 
                     else{
                         digit=int(song[i+1])-48;
                         for (int j=0; j<digit; j++){
-                            newSong+=toupper(song[i]);
+                            newSong+=toupper(song[i]); //sustained notes are capitalized
                         }
                         i=i+1+digit+1;
-                        }
                     }
-                    newSong+=tolower(song[i]);
-                    i+=2;
+                }
+                newSong+=tolower(song[i]); //normal notes are lower case
+                i+=2;
             }
             
             else if(song[i]=='/'){
